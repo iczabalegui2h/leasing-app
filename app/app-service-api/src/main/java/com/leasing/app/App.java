92a9b60925d8c6b55190cca63657ec442b72b648
Chambalegui <ivan.cruz@2hsoftware.com.mx>
2020-02-22 22:05:02 -0600

    estructura inicial del proyecto

---
+++ app/app-service-api/src/main/java/com/leasing/app/App.java
@@ -0,0 +1,13 @@
+package com.leasing.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
